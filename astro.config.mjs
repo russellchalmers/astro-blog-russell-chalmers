6cc5e9cd5e838f5252becad2f9c9dff1c7d4efaf
Russell Chalmers <russell.chalmers@gmail.com>
2025-07-25 21:45:50 +1000

    Getting there

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  site: 'http://localhost:4321',
+  //site: 'http://localhost:4321',
+  integrations: [tailwind()],
 });
