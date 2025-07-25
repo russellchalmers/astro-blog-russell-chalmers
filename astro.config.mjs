b207dc9769d987313bb7157208a1f38e03c90589
Russell Chalmers <russell.chalmers@gmail.com>
2025-07-25 21:55:17 +1000

    getting there 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
   //site: 'http://localhost:4321',
+  site: 'https://astro-blog-russell-chalmers.russell-chalmers.workers.dev',
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
